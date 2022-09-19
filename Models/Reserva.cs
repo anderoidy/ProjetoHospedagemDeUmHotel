3add6a9594cf1a1513072c65ed78c314acd0d7f5
anderoidy <anderoidy@gmail.com>
2022-09-19 06:24:12 -0300

    Created functions

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -11,10 +11,31 @@ namespace sistemaHospedagemHotel.Models
         Suite suite = new Suite();
         public int DiasReservados { get; set; }
 
+        public Reserva(int diasReservados) {
+            DiasReservados = diasReservados;
+        }
 
+
+        // Declaracao dos metodos 
         public void CadastrarHospedes(List<Pessoa> hospedes) {
             
         }
+
+        public void CadastrarSuite(Suite suite) {
+
+        }
+
+        public int ObterQuantidadeHospedes() {
+            return 0;
+        }
+
+        public decimal CalcularValorDiaria() {
+            return 0;
+        }
+
+
+
+
 
     }
 }
